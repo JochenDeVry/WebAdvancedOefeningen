5709485b0fff2cbe786a185a347b2ee601e74933
Jochen De Vry <jochen.devry@student.pxl.be>
2020-03-19 12:39:27 +0100

    finished oefening 2

--- les 3 - oefening2.php
+++ les 3 - oefening2.php
@@ -6,7 +6,26 @@
 </head>
 <body>
 <?php
-
+function maakRij($min, $max, $stap = 1){
+    if ($max >= $min){
+        for ($i = $min; $i <= $max; $i += $stap){
+            $array[] = $i;
+        }
+    } else{
+        for ($i = $min; $i >= $max; $i += $stap) {
+            $array[] = $i;
+        }
+    }
+    return $array;
+}
+?>
+<?php
+    $a = maakrij(5,1,-2);
+    $output = "[";
+    foreach ($a as $item){
+        $output .= $item.',';
+    }
+    print(substr($output,0,strlen($output) - 1)."]");
 ?>
 </body>
 </html>
